dfeeef98610b17c6252b92beac18b35fe0429a46
Christian Osada <christian.osada@lntinfotech.com>
2021-08-25 14:54:31 -0400

    refactoring user and book models/hibernate/repositories

--- src/main/java/com/bookbros/models/Book.java
+++ src/main/java/com/bookbros/models/Book.java
@@ -6,8 +6,13 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import lombok.Data;
 
 @Entity
+@Data
 @Table(name = "books")
 public class Book {
 	
@@ -15,6 +20,7 @@ public class Book {
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	@Column(name = "book_id")
 	private int id;
+	@NotNull @NotBlank
 	@Column(name = "title", nullable = false, unique = true)
 	private String title;
 	@Column(name = "author", nullable = false)
@@ -29,136 +35,4 @@ public class Book {
 	private String subjects;
 	@Column(name = "inventory")
 	private int inventory;
-	
-	public Book() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
-	public Book(String title, String author, double price, String published, String description,
-			String subjects, int inventory) {
-		super();
-		this.id = id;
-		this.title = title;
-		this.author = author;
-		this.price = price;
-		this.published = published;
-		this.description = description;
-		this.subjects = subjects;
-		this.inventory = inventory;
-	}
-
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public String getTitle() {
-		return title;
-	}
-	public void setTitle(String title) {
-		this.title = title;
-	}
-	public String getAuthor() {
-		return author;
-	}
-	public void setAuthor(String author) {
-		this.author = author;
-	}
-	public double getPrice() {
-		return price;
-	}
-	public void setPrice(double price) {
-		this.price = price;
-	}
-	public String getPublished() {
-		return published;
-	}
-	public void setPublished(String published) {
-		this.published = published;
-	}
-	public String getDescription() {
-		return description;
-	}
-	public void setDescription(String description) {
-		this.description = description;
-	}
-	public String getSubjects() {
-		return subjects;
-	}
-	public void setSubjects(String subjects) {
-		this.subjects = subjects;
-	}
-	public int getInventory() {
-		return inventory;
-	}
-	public void setInventory(int inventory) {
-		this.inventory = inventory;
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((author == null) ? 0 : author.hashCode());
-		result = prime * result + ((description == null) ? 0 : description.hashCode());
-		result = prime * result + id;
-		result = prime * result + inventory;
-		long temp;
-		temp = Double.doubleToLongBits(price);
-		result = prime * result + (int) (temp ^ (temp >>> 32));
-		result = prime * result + ((published == null) ? 0 : published.hashCode());
-		result = prime * result + ((subjects == null) ? 0 : subjects.hashCode());
-		result = prime * result + ((title == null) ? 0 : title.hashCode());
-		return result;
-	}
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Book other = (Book) obj;
-		if (author == null) {
-			if (other.author != null)
-				return false;
-		} else if (!author.equals(other.author))
-			return false;
-		if (description == null) {
-			if (other.description != null)
-				return false;
-		} else if (!description.equals(other.description))
-			return false;
-		if (id != other.id)
-			return false;
-		if (inventory != other.inventory)
-			return false;
-		if (Double.doubleToLongBits(price) != Double.doubleToLongBits(other.price))
-			return false;
-		if (published == null) {
-			if (other.published != null)
-				return false;
-		} else if (!published.equals(other.published))
-			return false;
-		if (subjects == null) {
-			if (other.subjects != null)
-				return false;
-		} else if (!subjects.equals(other.subjects))
-			return false;
-		if (title == null) {
-			if (other.title != null)
-				return false;
-		} else if (!title.equals(other.title))
-			return false;
-		return true;
-	}
-
-	@Override
-	public String toString() {
-		return "Book [id=" + id + ", title=" + title + ", author=" + author + ", price=" + price + ", published="
-				+ published + ", description=" + description + ", subjects=" + subjects + ", inventory=" + inventory
-				+ "]";
-	}
 }
