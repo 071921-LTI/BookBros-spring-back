27637c191ab64583900accf6ba3e21543fce19c9
Christian Osada <christian.osada@lntinfotech.com>
2021-08-25 16:58:43 -0400

    add impl interfaces

--- src/main/java/com/bookbros/services/UserService.java
+++ src/main/java/com/bookbros/services/UserService.java
@@ -1,62 +1,16 @@
 package com.bookbros.services;
 
 import java.util.List;
+import java.util.Optional;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import com.bookbros.daos.UserRepository;
+import com.bookbros.exceptions.UserNotFoundException;
 import com.bookbros.models.User;
 
-@Service
-public class UserService {
-	
-	private UserRepository ur;
-
-	@Autowired
-	public UserService(UserRepository ur) {
-		super();
-		this.ur = ur;
-	}
-	
-	@Transactional
-	public User getUserByUsername(String username) {
-		return ur.findByUsername(username);
-	}
-	
-	@Transactional
-	public List<User> getUsers() {
-		return ur.findAll();
-	}
-	
-	@Transactional
-	public boolean createUser(User u) {
-		ur.save(u);
-
-		if(ur.findById(u.getId()) == null) {
-			return false;
-		} else {
-			return true;
-		}
-	}
-	
-	@Transactional
-	public boolean deleteUser(User u) {
-		ur.delete(u);
-		if(ur.findById(u.getId()) == null) {
-			return true;
-		} else {
-			return false;
-		}
-	}
-	
-	public boolean updateUser(User u) {
-		ur.save(u);
-		if (ur.findByUsername(u.getUsername()) == u) {
-			return true;
-		} else {
-			return false;
-		}
-	}
+public interface UserService {
+	public abstract Optional<User> getById(int id) throws UserNotFoundException;
+	public abstract User getUserByUsername(String username) throws UserNotFoundException;
+	public abstract List<User> getUsers();
+	public abstract boolean createUser(User u);
+	public abstract boolean deleteUser(User u) throws UserNotFoundException;
+	public abstract boolean updateUser(User u) throws UserNotFoundException;
 }
