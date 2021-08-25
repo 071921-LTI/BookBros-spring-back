51cbb1c30d41c35ab8f99253c100387a7148ff91
Christian Osada <christian.osada@lntinfotech.com>
2021-08-25 16:25:19 -0400

    for got to add service tag

--- src/main/java/com/bookbros/services/BookService.java
+++ src/main/java/com/bookbros/services/BookService.java
@@ -3,11 +3,13 @@ package com.bookbros.services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.bookbros.daos.BookRepository;
 import com.bookbros.models.Book;
 
+@Service
 public class BookService {
 	
 	private BookRepository br;
