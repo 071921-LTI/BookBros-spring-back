fe84c3fdd3e035328185fc75c9bf468c209b397a
unknown <Lntinfotech@INFEDL31167.lntinfotech.com>
2021-08-26 17:04:27 -0400

    added the book api class in the api package. still needs to be completed but it is there.

--- src/main/java/com/bookbros/apis/BookAPI.java
+++ src/main/java/com/bookbros/apis/BookAPI.java
@@ -1,5 +1,24 @@
 package com.bookbros.apis;
 
-public class BookAPI {
+import java.util.List;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+import com.bookbros.models.Book;
+public class BookAPI {
+	
+	public Book findBookByTitle(String title) {
+		String url = "http://openlibrary.org/search.json?title=";
+		
+		
+		RestTemplate rt = new RestTemplate();
+		Book b = rt.getForObject(url + title, Book.class);
+		
+		return b;
+	}
+	
+	public List<Book> findBooksByAuthor(String author) {
+		
+	}
 }
