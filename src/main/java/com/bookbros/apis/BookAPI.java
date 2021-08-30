d5f52ef0f6694fb6da115af740d1d24cc28709bc
unknown <Lntinfotech@INFEDL31167.lntinfotech.com>
2021-08-30 14:29:22 -0400

    added a class to make API calls from the back-end and a test class to test out API calls.

--- src/main/java/com/bookbros/apis/BookAPI.java
+++ src/main/java/com/bookbros/apis/BookAPI.java
@@ -1,24 +1,97 @@
 package com.bookbros.apis;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
 import com.bookbros.models.Book;
+
+import org.json.*;
+//import org.json.simple.parser.JSONParser;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+
 public class BookAPI {
 	
-	public Book findBookByTitle(String title) {
-		String url = "http://openlibrary.org/search.json?title=";
+	public static Book findBookByTitle(String title) {
+		String titleUrl = "http://openlibrary.org/search.json?title=" + title;
+		
+		// get a jsonobject from the serch-by-title API
+		JSONObject titleJsonOjbect =  getJsonData(titleUrl);
+		
+		// get the book ISBN from the book-title API call
 		
 		
-		RestTemplate rt = new RestTemplate();
-		Book b = rt.getForObject(url + title, Book.class);
 		
-		return b;
+		
+		String isbn = "";
+		
+		// put the ISBN in the ISBN API call
+		String isbnUrl = "http://openlibrary.org/api/volumes/brief/isbn/" + isbn + ".json";
+		
+		// get the book info and convert it into a java Book object 
+		String jsonBookString = "";
+		JSONObject jsonBook = new JSONObject();
+		
+		String bookTitle;
+		String bookAuthor;
+		String bookPublished;
+		String bookDescription;
+		String bookSubjects;
+		
+		
+		
+//		RestTemplate rt = new RestTemplate();
+//		Book b = rt.getForObject(url + title, Book.class);
+//		
+//		return b;
 	}
 	
-	public List<Book> findBooksByAuthor(String author) {
+	public static JSONObject getJsonData(String jsonUrl) {
 		
+		JSONObject data_obj = null;
+		
+		try {
+			URL url = new URL(jsonUrl);
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.connect();
+            
+            int responsecode = conn.getResponseCode();
+            
+            String inline = "";
+            if (responsecode != 200) {
+                throw new RuntimeException("HttpResponseCode: " + responsecode);
+            }  else {
+            	
+            	
+                Scanner scanner = new Scanner(url.openStream());
+            	
+                while (scanner.hasNext()) {
+                    inline += scanner.nextLine();
+                }
+            	
+                scanner.close();
+                
+            }
+            
+//            JSONParser parse = new JSONParser();
+//            JSONObject data_obj = (JSONObject) parse.parse(inline);
+            	data_obj = new JSONObject(inline);
+            
+            
+            
+		} catch (Exception e) {
+            e.printStackTrace();
+        }
+		return data_obj;
 	}
+	
 }
