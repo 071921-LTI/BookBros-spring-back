e63fd1037a46beda7b7376c0df2a6ea31f0cc880
unknown <Lntinfotech@INFEDL31185.lntinfotech.com>
2021-08-31 13:18:30 -0500

    wrote createBook method is book service

--- src/main/java/com/bookbros/services/BookService.java
+++ src/main/java/com/bookbros/services/BookService.java
@@ -2,6 +2,7 @@ package com.bookbros.services;
 
 import java.util.List;
 
+import com.bookbros.dtos.Work;
 import com.bookbros.exceptions.BookNotFoundException;
 import com.bookbros.models.Book;
 
@@ -9,7 +10,7 @@ public interface BookService {
 	public abstract Book getByAuthorAndTitle(String author, String title) throws BookNotFoundException;
 	public abstract List<Book> getBooks();
 	public abstract List<Book> getBooksByAuthor(String author);
-	public abstract boolean createBook(Book b);
+	public abstract boolean createBook(Work work);
 	public abstract boolean deleteBook(Book b) throws BookNotFoundException;
 	public abstract boolean updateBook(Book b) throws BookNotFoundException;
 }
